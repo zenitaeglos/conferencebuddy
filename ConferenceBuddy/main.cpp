be540d3d6ad142eae343f48a5590fec730620222
zenitaeglos <aegloszenit@protonmail.com>
2019-08-28 22:30:38 +0200

    first commit. Setting the project

---
+++ ConferenceBuddy/main.cpp
@@ -0,0 +1,30 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QApplication>
+#include "src/mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    /*
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+    */
+
+    QApplication app(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return app.exec();
+}
