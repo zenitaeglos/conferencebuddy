9822ee373de828d3090d7ef150ac3070b2bfd872
zenitaeglos <aegloszenit@protonmail.com>
2020-02-07 20:04:53 +0100

    set first example on xml

--- ConferenceBuddy/dataconference/dataxmlformat.cpp
+++ ConferenceBuddy/dataconference/dataxmlformat.cpp
@@ -20,4 +20,26 @@ void DataXmlFormat::networkDataObtained(QNetworkReply *networkRelpy)
     qInfo() << "network info obtained";
     QByteArray xmlArray = networkRelpy->readAll();
     qDebug() << xmlArray;
+    QXmlStreamReader xmlReader(xmlArray);
+
+    while (!xmlReader.atEnd()) {
+        QXmlStreamReader::TokenType token = xmlReader.readNext();
+        if (token == QXmlStreamReader::StartElement) {
+            if (xmlReader.name() == "title") {
+                xmlReader.readNext();
+                qDebug() << "title" << xmlReader.text();
+            }
+            if (xmlReader.name() == "room") {
+                xmlReader.readNext();
+                qDebug() << "room" << xmlReader.text();
+            }
+            if (xmlReader.name() == "link") {
+                qDebug() << xmlReader.attributes().value("href");
+                xmlReader.readNext();
+
+
+                qDebug() << "link" << xmlReader.text() << xmlReader.attributes().value("href");
+            }
+        }
+    }
 }
--- ConferenceBuddy/dataconference/dataxmlformat.h
+++ ConferenceBuddy/dataconference/dataxmlformat.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QDebug>
+#include <QXmlStreamReader>
 #include "dataformat.h"
 
 class DataXmlFormat : public DataFormat
