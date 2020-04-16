3c6ef314df55a263ebf13987df80cc14707f62c4
zenitaeglos <aegloszenit@protonmail.com>
2020-04-16 09:54:35 +0200

    small tweaks

--- ConferenceBuddy/src/models/conferenceselectiontablemodel.cpp
+++ ConferenceBuddy/src/models/conferenceselectiontablemodel.cpp
@@ -7,21 +7,27 @@ ConferenceSelectionTableModel::ConferenceSelectionTableModel(QObject *parent) : 
 
 int ConferenceSelectionTableModel::rowCount(const QModelIndex &parent) const
 {
+    Q_UNUSED(parent);
     return conferences.count();
 }
 
 int ConferenceSelectionTableModel::columnCount(const QModelIndex &parent) const
 {
+    Q_UNUSED(parent);
     return 1;
 }
 
 QVariant ConferenceSelectionTableModel::data(const QModelIndex &index, int role) const
 {
+    Q_UNUSED(role);
     return conferences.at(index.row())["title"];
 }
 
 QVariant ConferenceSelectionTableModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
+    if (role == Qt::DisplayRole)
+        if (orientation == Qt::Horizontal)
+            return "Conferences";
     return QVariant();
 }
 
